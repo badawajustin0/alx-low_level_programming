7a29118dde31cdbd047868542094d74882faef68
badawajustin0 <j.magnetena@gmail.com>
2022-09-13 17:46:15 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
-/*
+/**
 * main - print putchar
 * Return: always 0
 */
-
 int main(void)
 {
 _putchar('_');
@@ -16,6 +14,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
