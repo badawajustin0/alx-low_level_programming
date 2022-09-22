bf42ac0f16a90fd8aaa992652c6bf78ba74a0145
badawajustin0 <j.magnetena@gmail.com>
2022-09-22 12:07:07 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,11 @@
  * returns positive number if s1 < s2
  */
 int _strcmp(char *s1, char *s2)
+{
 int i = 0, diff = 0;
 while (1)
 {
 if (s1[i] == '\0' && s2[i] == '\0')
-{
 break;
 else if (s1[i] == '\0')
 {
