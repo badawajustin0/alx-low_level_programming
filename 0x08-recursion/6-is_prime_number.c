8b65217824bcd462f2496a9a1f4488bf10051c2f
badawajustin0 <j.magnetena@gmail.com>
2022-09-29 19:42:42 -0700

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *check code
+ *
+ */
+int is_prime_number(int n)
+{
+int start = n / 2;
+if (n <= 1)
+	return (0);
+return (is_prime(n, start);
+}
+/*
+ *
+ *
+ *check code
+ *
+ */
+int is_prime(int n, int start)
+{
+if (start <= 1)
+return (1);
+else if (n % start == 0)
+return (0);
+return (is_prime(n, start - 1);
+}
